d1cbebe12d032f48c0318d1a27995b8c6069fa6b
Braian Delgado <braiandelgado4255@hotmail.com>
2022-07-12 11:01:03 -0300

    not found path & store lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuicklinkStrategy } from 'ngx-quicklink';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./website/website.module').then((e) => e.WebsiteModule),
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
 
